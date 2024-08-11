ffc9bb2d455ed1ef535006d680828119265e897d
Mike Eisner <eisner.michaelj@gmail.com>
2024-08-11 06:32:59 -0700

    Implemented Design System

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,22 +18,23 @@ module.exports = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
+        primary: "hsl(20.4, 62.1%, 55.5%)",
+        eastBay: "hsl(255.8, 25.9%, 40.2%)",
+        jagger: "hsl(269.1, 58.6%, 22.7%)",
+        neutral: "hsl(0, 0%, 0.8%)",
+        success: "hsl(81.2, 71.4%, 46.7%)",
+        caution: "hsl(49.3, 95.2%, 59.4%)",
+        danger: "hsl(346.9, 94.4%, 50.6%)",
+        info: "hsl(195.4, 94.5%, 57.1%)",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
@@ -43,14 +44,22 @@ module.exports = {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+      },
+      fontFamily: {
+        heading: ["Oswald", "sans-serif"],
+        subheading: ["Montserrat", "sans-serif"],
+        body: ["Cooper Hewitt", "sans-serif"],
+      },
+      fontSize: {
+        heading: "83px",
+        subheading: "23px",
+        body: "15px",
+      },
+      lineHeight: {
+        body: "1.6",
       },
       borderRadius: {
         lg: "var(--radius)",
