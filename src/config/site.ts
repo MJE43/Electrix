5aac191393b14bd90c2c535214a9821deda93b5a
Mike Eisner <eisner.michaelj@gmail.com>
2024-08-12 18:37:57 -0700

    added new logo

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,10 +3,9 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
-  author: "redpangilinan",
-  description:
-    "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
+  name: "Kyle Price Power and Electric",
+  author: "michaeleisner",
+  description: "Responsive Next.js website for Kyle Price Power and Electric.",
   keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
